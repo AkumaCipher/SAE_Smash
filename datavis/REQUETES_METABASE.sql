9dad00ee070402bad5e0b0ee1615882eb4c7432a
AkumaCipher <petitmamass59@gmail.com>
2022-12-22 13:51:14 +0100

    Update dataviz

--- datavis/REQUETES_METABASE.sql
+++ datavis/REQUETES_METABASE.sql
@@ -6,3 +6,6 @@ select sexe, count(id_personne) from personne group by sexe;
 
 --- Nombre de combats de chaque personnage --- 
 select id_perso, count(id_combat) from jouer_dans group by id_perso;
+
+--- Nombre de combats pour chaques Cartes ---
+select c.nom, count(c.id_carte) from combat as co inner join carte as c on c.id_carte = co.id_carte group by c.nom;
